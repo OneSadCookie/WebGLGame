6cba39b76cbe506a85e88db2a5df74bb73b463e9
OneSadCookie <onesadcookie@gmail.com>
2010-07-20 19:24:52 -0700

    fixes for texturing changes in more recent WebGL spec

--- web/scripts/webglutils.js
+++ web/scripts/webglutils.js
@@ -49,6 +49,10 @@ function initWebGL(canvasName)
                     // this may not be correct for all cases
                     gl.getProgramParameter = gl.getProgrami
                 }
+                
+                // I always want premultiplied imagery, you may not!
+                gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, 1)
+                
                 return gl
             }
         }
@@ -177,7 +181,7 @@ function loadTexture(url, gl, resman, closure)
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-        gl.texImage2D(gl.TEXTURE_2D, 0, image)
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image)
         
         closure(image, texture)
         resman.loaded('Texture of ' + url)
