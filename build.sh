92d9dcb76ee7c7ac95ddf06429ce2d1a89250b54
Keith Bauer <onesadcookie@gmail.com>
2014-08-29 23:01:15 +1200

    replace RubyCocoa tile packer which 10.10 kills with a swift equivalent (after much, much pain with swift)

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./tilepacker.rb PlanetCutePNG tiles web/images/tilemetrics.json
+./tilepacker.swift PlanetCutePNG tiles web/images/tilemetrics.json
 sips -s format png tiles.tiff --out web/images/tiles.png
 rm tiles.tiff
 cp PlanetCutePNG/SpeechBubble.png web/images/speech-up-right.png
---
+++ tilepacker.swift
@@ -0,0 +1,128 @@
+#!/usr/bin/xcrun swift
+
+import Cocoa
+
+extension NSDirectoryEnumerator : SequenceType, GeneratorType
+{
+    public typealias GeneratorType = NSDirectoryEnumerator
+    public func generate() -> GeneratorType { return self }
+    
+    public typealias Element = NSString
+    public func next() -> Element? { return nextObject() as? Element }
+}
+
+extension NSOutputStream : OutputStreamType
+{
+    public func write(string: String)
+    {
+        string.withCString()
+        { [weak self] (s: UnsafePointer<Int8>) -> () in
+            let bytes = UnsafePointer<UInt8>(s)
+            let len = Int(strlen(s))
+            self!.write(bytes, maxLength: len)
+        }
+    }
+}
+
+struct TileFile
+{
+    let path: String
+    let image: NSBitmapImageRep
+}
+
+var err:NSError?
+let dir = String.fromCString(C_ARGV[1])!
+let out_tiff = String.fromCString(C_ARGV[2])!
+let out_json = String.fromCString(C_ARGV[3])!
+let fm = NSFileManager.defaultManager()
+let png_re = NSRegularExpression(pattern: "^(.*)\\.png$", options: nil, error: &err)
+var images:Dictionary<String, TileFile> = [:]
+for filename in fm.enumeratorAtPath(dir)
+{
+    let string_range = NSRange(location: 0, length: filename.length)
+    let match:NSTextCheckingResult? = png_re.firstMatchInString(filename, options: nil, range: string_range)
+    if let m = match
+    {
+        let path = dir.stringByAppendingPathComponent(filename)
+        images[filename.substringWithRange(m.rangeAtIndex(1))] = TileFile(path: path, image: NSBitmapImageRep(data: NSData(contentsOfFile: path)))
+    }
+}
+
+var tile_size: (Int, Int)?
+for (k, v) in images
+{
+    if let (w, h) = tile_size
+    {
+        if w != v.image.pixelsWide || h != v.image.pixelsHigh
+        {
+            println("Image \(k) is wrong size(\(v.image.pixelsWide) × \(v.image.pixelsHigh)); discarding.")
+            images.removeValueForKey(k)
+        }
+    }
+    else
+    {
+        tile_size = (v.image.pixelsWide, v.image.pixelsHigh)
+    }
+    // discard DPI
+    v.image.size = NSSize(width: v.image.pixelsWide, height: v.image.pixelsHigh)
+}
+assert(tile_size != nil)
+
+let PAD = 2
+
+let s = sqrt(Double(images.count))
+let (tile_width, tile_height) = tile_size!
+var tiles_across = Int(ceil(s))
+var tiles_down = Int(floor(s))
+var total_tiles = tiles_across * tiles_down
+var pixel_width = tiles_across * (tile_width + PAD) + PAD
+var pixel_height = tiles_down * (tile_height + PAD) + PAD
+while total_tiles < images.count || pixel_width < pixel_height
+{
+    if total_tiles < images.count
+    {
+        tiles_across += 1
+    }
+    else if pixel_width < pixel_height
+    {
+        tiles_across += 1
+        tiles_down -= 1
+    }
+    total_tiles = tiles_across * tiles_down
+    pixel_width = tiles_across * (tile_width + PAD) + PAD
+    pixel_height = tiles_down * (tile_height + PAD) + PAD
+}
+println("Using \(tiles_across) × \(tiles_down); \(total_tiles) total (\(images.count) filled); \(pixel_width)px × \(pixel_height)px")
+
+let large_image_rep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: pixel_width, pixelsHigh: pixel_height, bitsPerSample: 8, samplesPerPixel: 4, hasAlpha: true, isPlanar: false, colorSpaceName: NSDeviceRGBColorSpace, bytesPerRow: pixel_width * 4, bitsPerPixel: 32)
+NSGraphicsContext.setCurrentContext(NSGraphicsContext(bitmapImageRep: large_image_rep))
+var x = 0, y = 0
+var first = true
+var f = NSOutputStream(toFileAtPath: out_json, append: false)
+f.open()
+println("{", &f)
+for (k, v) in images
+{
+    let x_px = x * (tile_width + PAD) + PAD
+    let y_px = y * (tile_height + PAD) + PAD
+    v.image.drawAtPoint(NSPoint(x: x_px, y: y_px))
+    if first
+    {
+        first = false
+    }
+    else
+    {
+        println(",", &f)
+    }
+    x += 1
+    if x >= tiles_across
+    {
+        x = 0
+        y += 1
+    }
+    print("    \"\(k)\": {\n        \"x\": \(x_px),\n        \"y\": \(y_px),\n        \"w\": \(tile_width),\n        \"h\": \(tile_height)\n    }", &f)
+}
+println("\n}", &f)
+f.close()
+NSGraphicsContext.currentContext().flushGraphics()
+large_image_rep.TIFFRepresentation.writeToFile(out_tiff + ".tiff", atomically: true)
